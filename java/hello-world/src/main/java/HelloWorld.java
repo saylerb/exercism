88699b0966c48619bf173b784e9c9f71a457420c
Brian Sayler <sayler.b@gmail.com>
2016-11-26 00:08:43 -0500

    refactorwith private method

--- java/hello-world/src/main/java/HelloWorld.java
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
     public static String hello(String name) {
-        if(name == null || "".equals(name)) {
-            name = "World";
-        }
-        return "Hello, " + name + "!";
+        String whom = isBlank(name) ? "World" : name;
+        return "Hello, " + whom + "!";
+    }
+
+    private static boolean isBlank(String string) {
+        return string == null || "".equals(string);
     }
 }

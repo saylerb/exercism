747f1d395c2e039f592b0c547747d4f0ce4c3311
Brian Sayler <sayler.b@gmail.com>
2017-02-05 13:31:41 -0700

    complete ruby grains

---
+++ ruby/grains/grains.rb
@@ -0,0 +1,19 @@
+module Grains
+  def self.square(square_number)
+    unless square_number.between?(1, 64)
+      raise ArgumentError, 'square number must be postive between 1 and 64'
+    end
+
+    return 2 ** (square_number - 1)
+  end
+
+  def self.total
+    (1..64).to_a.reduce(0) do |sum, square_number|
+      sum += square(square_number)
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end

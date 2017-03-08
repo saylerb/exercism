84f0993945d75ec6be30fd2c2a23fc75c74496c7
Brian Sayler <sayler.b@gmail.com>
2017-03-08 11:33:18 -0700

    complete first iteration of Pangram in java

---
+++ java/pangram/notes.md
@@ -0,0 +1,9 @@
+## Solution notes
+
+* String manipulation in java is hard!
+* remember `==` compares object equality in java (not value)
+* To compare the equality of strings, use the String.equals()
+
+## TODO:
+* Current solution has nested for loops (bad!)
+* Can I refactor so that I pass over the string only once?
--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
+
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Method has not been implemented yet.");
+        String[] alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+        String[] inputList = input.toLowerCase().split("");
+        int result = 0;
+
+        for (String letter : alphabet) {
+            for (String character : inputList) {
+                if (letter.equals(character)) {
+                    result = result + 1;
+                    break;
+                }
+            }
+        }
+
+        if (result >= 26){
+            return true;
+        } else {
+            return false;
+        }
+
     }
+
 }

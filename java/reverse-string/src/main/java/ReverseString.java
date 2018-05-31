a47d80c102ff7cdb9064165993d29c1d2ee1ed65
Brian Sayler <sayler.b@gmail.com>
2018-05-31 11:11:48 -0500

    use StringBuilder in ReverseString

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,14 +1,7 @@
 class ReverseString {
     String reverse(String inputString) {
-        char[] charArray = inputString.toCharArray();
+        StringBuilder newString = new StringBuilder(inputString);
 
-        StringBuilder newString = new StringBuilder();
-
-        for (int i = charArray.length - 1; i >= 0; i--) {
-            newString.append(charArray[i]);
-        }
-
-        return newString.toString();
+        return newString.reverse().toString();
     }
-
 }

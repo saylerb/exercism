90f3420e0a7bcd5e843ec0cdc9265c52e4b1cb99
Brian Sayler <sayler.b@gmail.com>
2018-06-01 23:36:34 -0500

    extract armstrong num solution into methods

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,8 +1,15 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
+        List<Integer> listOfDigits = generateListOfDigits(numberToCheck);
+
+        return sumArmstrongDigits(listOfDigits) == numberToCheck;
+    }
+
+    private List<Integer> generateListOfDigits(int numberToCheck) {
         int workingDigit = numberToCheck;
 
         ArrayList<Integer> listOfDigits = new ArrayList<>();
@@ -12,13 +19,13 @@ class ArmstrongNumbers {
             workingDigit = workingDigit / 10;
         }
 
-        int totalDigits = listOfDigits.size();
+        return listOfDigits;
+    }
 
-        int sum = listOfDigits
-            .stream()
-            .reduce(0, (a, b) -> a + (int) Math.pow(b, totalDigits));
-
-        return sum == numberToCheck;
+    private int sumArmstrongDigits(List<Integer> listOfDigits) {
+        return listOfDigits
+                .stream()
+                .reduce(0, (a, b) -> a + (int) Math.pow(b, listOfDigits.size()));
     }
 
 }

41876a9fa5d9d5218701d34568b222022fb8a141
Brian Sayler <sayler.b@gmail.com>
2018-11-07 21:07:09 -0600

    raindrops javascript

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,19 @@
+export default function Raindrops() {
+  this.convert = function convert(number) {
+    let result = '';
+
+    if (number % 3 === 0) {
+      result += 'Pling';
+    }
+
+    if (number % 5 === 0) {
+      result += 'Plang';
+    }
+
+    if (number % 7 === 0) {
+      result += 'Plong';
+    }
+
+    return result === '' ? number.toString() : result;
+  };
+}

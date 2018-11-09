1493c3b2afd76916ef233c98caf49c108a290476
Brian Sayler <sayler.b@gmail.com>
2018-11-08 23:59:29 -0600

    fix linting errors

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,13 +1,9 @@
-const transform = (data) => {
-    return Object.keys(data).reduce((acc, key) => {
-        const values = data[key]
-
-        values.forEach(value => {
-            acc[value.toLowerCase()] = parseInt(key)
-        })
-
-        return acc
-    }, {})
-}
+const transform = data => (
+  Object.keys(data).reduce((acc, key) => {
+    const values = data[key];
+    values.forEach(value => acc[value.toLowerCase()] = parseInt(key))
+    return acc;
+  }, {})
+)
 
 export default transform

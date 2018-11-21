3eb0c7f2b71db991a55e29dd60639a4856b14d87
Brian Sayler <sayler.b@gmail.com>
2018-11-21 13:23:33 -0500

    add validation for key cipher

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -9,9 +9,31 @@ function generateRandomString() {
     .join('');
 }
 
+function isNumericString(string) {
+  return !Number.isNaN(Number.parseInt(string, 10));
+}
+
+function isAllLowerCase(string) {
+  return string === string.toLowerCase();
+}
+
+function containsWhiteSpace(string) {
+  return string.trim() !== string;
+}
+
+function containsHyphen(string) {
+  return string.replace(/-/g, '') !== string;
+}
+
 export class Cipher {
-  constructor() {
-    this.key = generateRandomString();
+  constructor(key) {
+    if (key || key === '') {
+      this.constructor.validate(key); // calling a static method
+      this.key = key;
+    } else {
+      this.key = generateRandomString();
+    }
+
     this.shifts = this.calculateShifts();
   }
 
@@ -30,5 +52,16 @@ export class Cipher {
     return decodeString.split('').map((char, index) => String.fromCharCode(
       char.charCodeAt() - this.shifts[index],
     )).join('');
+  }
+
+  static validate(key) {
+    if (key.length === 0
+      || isNumericString(key)
+      || !isAllLowerCase(key)
+      || containsWhiteSpace(key)
+      || containsHyphen(key)
+    ) {
+      throw new Error('Bad key');
+    }
   }
 }

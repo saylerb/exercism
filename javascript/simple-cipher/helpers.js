28631387d3eeace6ddd51f1aadd5f7f326951174
Brian Sayler <sayler.b@gmail.com>
2018-11-30 18:11:51 -0600

    clean up cipher

---
+++ javascript/simple-cipher/helpers.js
@@ -0,0 +1,42 @@
+export function randomLetter() {
+  return String.fromCharCode(Math.floor(Math.random() * 25) + 98);
+}
+
+export function generateRandomString() {
+  return [...(null, { length: 100 })]
+    .map(() => randomLetter())
+    .join('');
+}
+
+export function isNumericString(string) {
+  return !Number.isNaN(Number.parseInt(string, 10));
+}
+
+export function nonLowerCase(string) {
+  return string !== string.toLowerCase();
+}
+
+export function containsWhiteSpace(string) {
+  return string.trim() !== string;
+}
+
+export function containsHyphen(string) {
+  return string.replace(/-/g, '') !== string;
+}
+
+export function letterToIndex(letter) {
+  return letter.charCodeAt() - 97;
+}
+
+export function indexToLetter(index) {
+  return String.fromCharCode(index + 97);
+}
+
+export function applyShift(letter, shiftFunction) {
+  const startIndex = letterToIndex(letter);
+  const shiftedIndex = shiftFunction(startIndex);
+  return indexToLetter(shiftedIndex);
+}
+
+export const positiveShift = shift => start => (start + shift) % 26;
+export const negativeShift = shift => start => ((start - shift) + 26) % 26;
--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,50 +1,19 @@
-function randomLetter() {
-  return String.fromCharCode(Math.floor(Math.random() * 25) + 98);
-}
-
-function generateRandomString() {
-  return [...(null, { length: 100 })]
-    .map(() => randomLetter())
-    .join('');
-}
-
-function isNumericString(string) {
-  return !Number.isNaN(Number.parseInt(string, 10));
-}
-
-function nonLowerCase(string) {
-  return string !== string.toLowerCase();
-}
-
-function containsWhiteSpace(string) {
-  return string.trim() !== string;
-}
-
-function containsHyphen(string) {
-  return string.replace(/-/g, '') !== string;
-}
-
-function letterToIndex(letter) {
-  return letter.charCodeAt() - 97;
-}
-
-function indexToLetter(index) {
-  return String.fromCharCode(index + 97);
-}
-
-function applyShift(letter, shiftFunction) {
-  const startIndex = letterToIndex(letter);
-  const shiftedIndex = shiftFunction(startIndex);
-  return indexToLetter(shiftedIndex);
-}
-
-const positiveShift = shift => start => (start + shift) % 26;
-const negativeShift = shift => start => ((start - shift) + 26) % 26;
+import {
+  isNumericString,
+  nonLowerCase,
+  containsWhiteSpace,
+  containsHyphen,
+  generateRandomString,
+  letterToIndex,
+  applyShift,
+  positiveShift,
+  negativeShift,
+} from './helpers';
 
 export class Cipher {
   constructor(key) {
     if (key || key === '') {
-      this.constructor.validate(key); // calling a static method
+      Cipher.validate(key); // calling a static method
       this.key = key;
     } else {
       this.key = generateRandomString();

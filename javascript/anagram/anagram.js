8b9dcdc0500ac08a8983771513a08ad6ad9d71b2
Brian Sayler <sayler.b@gmail.com>
2018-12-15 18:16:43 -0600

    pass two pairs

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -14,13 +14,15 @@ class Anagram {
   matches(list) {
     const pairs = list.map(word => [word, sortCharacters(word)]);
 
-    return pairs.filter(pair => this.isAnagram(pair)).map(pair => pair[0]);
+    return pairs
+      .filter(pair => this.isAnagram(pair[0], pair[1]))
+      .map(pair => pair[0]);
   }
 
-  isAnagram(pair) {
+  isAnagram(originalWord, sortedWord) {
     return (
-      this.subject.toLowerCase() !== pair[0].toLowerCase() &&
-      sortCharacters(this.subject) === pair[1]
+      this.subject.toLowerCase() !== originalWord.toLowerCase() &&
+      sortCharacters(this.subject) === sortedWord
     );
   }
 }

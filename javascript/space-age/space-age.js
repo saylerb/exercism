2f4fb7c0a43824c010c9a60038d4c2dc7b8b6f61
Brian Sayler <sayler.b@gmail.com>
2018-12-19 22:36:51 -0600

    make round static since it doesn't use this

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -4,14 +4,14 @@ export class SpaceAge {
   }
 
   onEarth() {
-    return this.round(this.seconds / (60 * 60 * 24 * 365.25));
+    return SpaceAge.round(this.seconds / (60 * 60 * 24 * 365.25));
   }
 
   onMercury() {
-    return this.round(this.onEarth() / 0.2408467);
+    return SpaceAge.round(this.onEarth() / 0.2408467);
   }
 
-  round(number) {
+  static round(number) {
     return Math.round(number * 100) / 100;
   }
 }

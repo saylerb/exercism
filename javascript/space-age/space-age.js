3ac9607c47d5d6a2a07d7541d078a13490fa158e
Brian Sayler <sayler.b@gmail.com>
2021-02-10 13:53:55 -0700

    convert OO solution to function solution

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,59 +1,26 @@
 export function age(planet, seconds) {
-  const [first, ...rest] = planet;
-
-  const methodToCall = `on${first.toUpperCase()}${rest.join("")}`;
-
-  return new SpaceAge(seconds)[methodToCall]();
+  return round(getEarthYears(seconds) / orbitalDurationInEarthYears(planet));
 }
 
-export class SpaceAge {
-  constructor(seconds) {
-    this.seconds = seconds;
-    this.currentSeconds = null;
-    this.earthYears = null;
-  }
-
-  getSeconds() {
-    if (this.seconds !== this.currentSeconds) {
-      this.currentSeconds = this.seconds;
-      this.earthYears = this.seconds / (60 * 60 * 24 * 365.25);
-    }
-    return this.earthYears;
-  }
-
-  onEarth() {
-    return SpaceAge.round(this.getSeconds());
-  }
-
-  onMercury() {
-    return SpaceAge.round(this.getSeconds() / 0.2408467);
-  }
-
-  onVenus() {
-    return SpaceAge.round(this.getSeconds() / 0.61519726);
-  }
-
-  onMars() {
-    return SpaceAge.round(this.getSeconds() / 1.8808158);
-  }
-
-  onJupiter() {
-    return SpaceAge.round(this.getSeconds() / 11.862615);
-  }
-
-  onSaturn() {
-    return SpaceAge.round(this.getSeconds() / 29.447498);
-  }
-
-  onUranus() {
-    return SpaceAge.round(this.getSeconds() / 84.016846);
-  }
-
-  onNeptune() {
-    return SpaceAge.round(this.getSeconds() / 164.79132);
-  }
-
-  static round(num) {
-    return Math.round(num * 100) / 100;
-  }
+function orbitalDurationInEarthYears(planetName) {
+  return orbitalDurationMap.get(planetName);
 }
+
+function getEarthYears(seconds) {
+  return seconds / (60 * 60 * 24 * 365.25);
+}
+
+function round(num) {
+  return Math.round(num * 100) / 100;
+}
+
+const orbitalDurationMap = new Map([
+  ["earth", 1],
+  ["mercury", 0.2408467],
+  ["venus", 0.61519726],
+  ["mars", 1.8808158],
+  ["jupiter", 11.862615],
+  ["saturn", 29.447498],
+  ["uranus", 84.016846],
+  ["neptune", 164.79132],
+]);
